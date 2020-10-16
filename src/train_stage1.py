5f4a5de25d07962e3214976c20f60a48309644f0
lknf8888 <72058680+lknf8888@users.noreply.github.com>
2020-10-16 18:23:00 +0700

    Update train_stage1.py

--- src/train_stage1.py
+++ src/train_stage1.py
@@ -16,7 +16,7 @@ class Reshape(nn.Module):
 
     def forward(self, x):
         return x.view(self.shape)
-        
+
 n_class = int(os.getenv('N_CLASS'))
 epochs = int(os.getenv('N_EPOCH'))
 batch_size = int(os.getenv('BATCH_SIZE'))
@@ -32,6 +32,7 @@ multi_gpus = device==torch.device('cuda') and torch.cuda.device_count() > 1
 '''-----------------MODEL------------------------'''
 enet_model = 'efficientnet-b0'
 feat_size = 1280
+use_bias = True
 
 model = nn.Sequential(
     EfficientNet.from_pretrained(enet_model, include_top=False,),
