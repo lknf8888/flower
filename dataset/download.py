ffaba6049e323424e9c5b46ef4dae9f0accdeb1c
lknf8888 <72058680+lknf8888@users.noreply.github.com>
2020-10-16 19:00:36 +0700

    Update download.py

--- dataset/download.py
+++ dataset/download.py
@@ -2,9 +2,9 @@ import gdown
 import os
 
 url = 'https://drive.google.com/uc?id=%s'%os.getenv('DATASET')
-output = os.path.join('/app',os.getenv('DATA_FOLDER'))
+output = os.path.join('/app',os.getenv('DATA_DIR'))
 gdown.download(url, '/app/flower_photos.tgz', quiet=False)
 
-os.system('mkdir %s'%output)
+os.makedirs(output)
 os.system('tar -xzvf /app/flower_photos.tgz -C %s'%output)
 os.system('ls -al %s'%output)
