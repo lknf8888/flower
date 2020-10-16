62d357e7c7eb5d89afb992a36e38fdbeffe66959
lknf8888 <72058680+lknf8888@users.noreply.github.com>
2020-10-16 17:44:01 +0700

    Add files via upload

---
+++ src/flower.py
@@ -0,0 +1,76 @@
+import os
+from PIL import Image
+
+from torch.utils.data import Dataset
+from torchvision import transforms
+
+class Flower(Dataset):
+
+    def __init__(self, path, shape=[224,224], pad=32, normalize=False, mode='val'):
+
+        filenames = []
+        labels = []
+
+        folders = os.listdir(path)
+
+        for f in folders:
+            foldername = os.path.join(path,f)
+            if not if os.path.isdir(foldername):
+                continue
+
+            files = os.listdir(foldername)
+
+            label = int(f)
+            for name in files:
+                fname = os.path.join(foldername, name)
+
+                labels.append(label)
+                filenames.append(fname)
+
+        self.filenames = filenames
+        self.labels = labels
+        self.split = mode
+
+        padded_size = [shape[0]+pad, shape[1]+pad]
+        if normalize:
+            self.preprocessor = {
+                'train':transforms.Compose([
+                    transforms.Resize(padded_size),
+                    transforms.RandomCrop(shape),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+                ]),
+                'test':transforms.Compose([
+                    transforms.Resize(shape),
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         std=[0.229, 0.224, 0.225])
+                ])
+            }
+        else:
+            self.preprocessor = {
+                'train':transforms.Compose([
+                    transforms.Resize(padded_size),
+                    transforms.RandomCrop(shape),
+                    transforms.ToTensor(),
+                ]),
+                'test':transforms.Compose([
+                    transforms.Resize(shape),
+                    transforms.ToTensor(),
+                ])
+            }
+
+
+    def preprocess(self, image):
+        split = 'train' if self.split=='train' else 'test'
+        transform = self.preprocessor[split]
+        return transform(image)
+
+    def __len__(self):
+        return len(self.filenames)
+
+    def __getitem__(self, i):
+        fname, label = self.filenames[i], self.labels[i]
+        image = self.preprocess(Image.open(fname).convert('RGB'))
+        return image, label
---
+++ src/train_stage2.py
@@ -0,0 +1,72 @@
+from comet_ml import Experiment
+import os
+
+import torch
+from torch.nn.functional import cross_entropy
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import DataLoader
+
+from flower import Flower
+
+
+n_class = int(os.getenv['N_CLASS'])
+epochs = int(os.getenv['N_EPOCH'])
+batch_size = int(os.getenv['BATCH_SIZE'])
+data_dir = os.getenv['DATA_FOLDER']
+save_interval = int(os.getenv['SAVE_INTERVAL'])
+save_path = os.getenv['SAVE_PATH']
+
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+multi_gpus = device==torch.device('cuda') and torch.cuda.device_count() > 1
+
+
+'''-----------------MODEL------------------------'''
+enet_model = 'efficientnet-b0'
+feat_size = 1280
+
+model = nn.Sequential(
+    EfficientNet.from_pretrained(enet_model, include_top=False,),
+    Reshape(-1,feat_size),
+    nn.Linear(feat_size, n_class, bias=use_bias)
+)
+
+checkpoint = save_path+'/stage1.pth'
+model.load_state_dict(torch.load(checkpoint, map_location=device) )
+
+if multi_gpus:
+    model = torch.nn.DataParallel(model, device_ids = range(torch.cuda.device_count()))
+model.to(device)
+
+'''-----------------MODEL------------------------'''
+
+'''-------------------TRAINING----------------------------'''
+
+dataset_path = 'dataset/%s'%data_dir
+
+trainset = Johnson(path=dataset_path,mode='train')
+train_loader = DataLoader(dataset=trainset,
+                          batch_size=batch_size,
+                          shuffle=True, num_workers=8)
+
+for e in range(epochs):
+
+    model.train()
+
+    for i, (x,y) in enumerate(train_loader):
+        x = x.to(device)
+        y = y.to(device)
+
+        logits = model(x_query)
+        loss = cross_entropy(logits,y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print(e, loss.item())
+
+    if e%save_interval == 0:
+        torch.save(model.state_dict(), os.path.join(save_path, 'stage2_epoch-{}'.format(e) + '.pth'))
+    if e==epochs-1:
+        torch.save(model.state_dict(), os.path.join(save_path, 'stage2.pth'))
